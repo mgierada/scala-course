08b31656b104c6181531d30b77b8bc90c1b7750a
Maciej Gierada <maciej@braidwell.io>
2023-05-29 16:33:55 +0200

    lesson done

--- src/main/scala/com/rockthejvm/part2effects/ZIOErrorHandling.scala
+++ src/main/scala/com/rockthejvm/part2effects/ZIOErrorHandling.scala
@@ -28,11 +28,13 @@ object ZIOErrorHandling extends ZIOAppDefault {
   }
 
   // effectfully catch errors
-  val catchError = anAttempt.catchAll(e => ZIO.succeed(s"Returning a different value because $e"))
+  val catchError = anAttempt.catchAll(e =>
+    ZIO.succeed(s"Returning a different value because $e")
+  )
   // this way we can catch selected errors and return a different value or thrown something else
   val catchSelectiveErrors = anAttempt.catchSome {
     case e: RuntimeException => ZIO.succeed(s"Ignoring runtime exceptions: $e")
-    case _ => ZIO.succeed("Ignoring everything else")
+    case _                   => ZIO.succeed("Ignoring everything else")
     // get fail as well
     // case _ => ZIO.fail("Ignoring everything else")
   }
@@ -40,7 +42,10 @@ object ZIOErrorHandling extends ZIOAppDefault {
   // chain effects
   val aBetterAttempt = anAttempt.orElse(ZIO.succeed(56))
   // fold: handle both success and failure
-  val handleBoth: ZIO[Any, Nothing, String] = anAttempt.fold(ex => s"Something bad happened: $ex", value => s"Length of the string was $value")
+  val handleBoth: ZIO[Any, Nothing, String] = anAttempt.fold(
+    ex => s"Something bad happened: $ex",
+    value => s"Length of the string was $value"
+  )
   // effectful fold: foldZIO cool way to control error channels
   val handleBoth_v2 = anAttempt.foldZIO(
     ex => ZIO.succeed(s"Something bad happened: $ex"),
@@ -51,7 +56,9 @@ object ZIOErrorHandling extends ZIOAppDefault {
     Conversions between Option/Try/Either to ZIO
    */
 
-  val aTryToZIO: ZIO[Any, Throwable, Int] = ZIO.fromTry(Try(42 / 0))  // can fail with Throwable - natural channel of error
+  val aTryToZIO: ZIO[Any, Throwable, Int] = ZIO.fromTry(
+    Try(42 / 0)
+  ) // can fail with Throwable - natural channel of error
 
   // either -> ZIO
   // IO is the same as ZIO with any environment
@@ -66,38 +73,41 @@ object ZIOErrorHandling extends ZIOAppDefault {
   // option -> ZIO
   val anOption: ZIO[Any, Option[Nothing], Int] = ZIO.fromOption(Some(42))
 
-  /**
-   * Exercise: implement a version of fromTry, fromOption, fromEither, either, absolve
-   * using fold and foldZIO
-   */
+  /** Exercise: implement a version of fromTry, fromOption, fromEither, either,
+    * absolve using fold and foldZIO
+    */
 
   def try2ZIO[A](aTry: Try[A]): Task[A] = aTry match {
     case Failure(exception) => ZIO.fail(exception)
-    case Success(value) => ZIO.succeed(value)
+    case Success(value)     => ZIO.succeed(value)
   }
 
-  def either2ZIO[A, B](anEither: Either[A, B]): ZIO[Any, A, B] = anEither match {
-    case Left(value) => ZIO.fail(value)
-    case Right(value) => ZIO.succeed(value)
-  }
+  def either2ZIO[A, B](anEither: Either[A, B]): ZIO[Any, A, B] =
+    anEither match {
+      case Left(value)  => ZIO.fail(value)
+      case Right(value) => ZIO.succeed(value)
+    }
 
-  def option2ZIO[A](anOption: Option[A]): ZIO[Any, Option[Nothing], A] = anOption match {
-    case Some(value) => ZIO.succeed(value)
-    case None => ZIO.fail(None)
-  }
+  def option2ZIO[A](anOption: Option[A]): ZIO[Any, Option[Nothing], A] =
+    anOption match {
+      case Some(value) => ZIO.succeed(value)
+      case None        => ZIO.fail(None)
+    }
 
-  def zio2zioEither[R,A,B](zio: ZIO[R, A, B]): ZIO[R, Nothing, Either[A, B]] = zio.foldZIO(
-    error => ZIO.succeed(Left(error)),
-    value => ZIO.succeed(Right(value))
-  )
+  def zio2zioEither[R, A, B](zio: ZIO[R, A, B]): ZIO[R, Nothing, Either[A, B]] =
+    zio.foldZIO(
+      error => ZIO.succeed(Left(error)),
+      value => ZIO.succeed(Right(value))
+    )
 
-  def absolveZIO[R,A,B](zio: ZIO[R, Nothing, Either[A, B]]): ZIO[R,A,B] = zio.flatMap {
-    case Left(e) => ZIO.fail(e)
-    case Right(v) => ZIO.succeed(v)
-  }
+  def absolveZIO[R, A, B](zio: ZIO[R, Nothing, Either[A, B]]): ZIO[R, A, B] =
+    zio.flatMap {
+      case Left(e)  => ZIO.fail(e)
+      case Right(v) => ZIO.succeed(v)
+    }
 
   // Second video
-  
+
   // ERRORS VS DEFECTS
 
   /*
@@ -113,9 +123,9 @@ object ZIOErrorHandling extends ZIOAppDefault {
   val divisionByZero: UIO[Int] = ZIO.succeed(1 / 0)
 
   val failedInt: ZIO[Any, String, Int] = ZIO.fail("I failed!")
-  val failureCauseExposed: ZIO[Any, Cause[String], Int] = failedInt.sandbox
-  val failureCauseHidden: ZIO[Any, String, Int] = failureCauseExposed.unsandbox
-  // fold with cause
+  val failureCauseExposed: ZIO[Any, Cause[String], Int] = failedInt.sandbox // sandbox exposes the cause for the entire ZIO channel
+  val failureCauseHidden: ZIO[Any, String, Int] = failureCauseExposed.unsandbox // just reverse
+  // fold with cause - the entire error channel
   val foldedWithCause = failedInt.foldCause(
     cause => s"this failed with ${cause.defects}",
     value => s"this succeeded with $value"
@@ -137,19 +147,22 @@ object ZIOErrorHandling extends ZIOAppDefault {
   val endpointCallWithDefects: ZIO[Any, Nothing, String] =
     callHTTPEndpoint("rockthejvm.com").orDie // all errors are now defects
 
-  // refining the error channel
+  
+  // refining
   def callHTTPEndpointWideError(url: String): ZIO[Any, Exception, String] =
     ZIO.fail(new IOException("No internet!!"))
 
+  // refining the error channel - some of errors are defects now IOException() the rest are errors
   def callHTTPEndpoint_v2(url: String): ZIO[Any, IOException, String] =
     callHTTPEndpointWideError(url).refineOrDie[IOException] {
-      case _: NoRouteToHostException => new IOException(s"No route to host to $url, can't fetch page")
+      case _: NoRouteToHostException =>
+        new IOException(s"No route to host to $url, can't fetch page")
       case e: IOException => e
     }
 
   // reverse: turn defects into the error channel
-  val endpointCallWithError = endpointCallWithDefects.unrefine {
-    case e => e.getMessage
+  val endpointCallWithError = endpointCallWithDefects.unrefine { case e =>
+    e.getMessage
   }
 
   /*
@@ -165,35 +178,36 @@ object ZIOErrorHandling extends ZIOAppDefault {
   } yield (page, rowsAffected) // lost type safety
   /*
     Solutions:
-      - design an error model
-      - use Scala 3 union types
+      - design an error model eg. using trait
+      - use Scala 3 union types eg. val combined: ZIO[Any, IndexError | DbError, (String, Int)]
       - .mapError to some common error type
-  */
-
-  /**
-   *  Exercises
    */
+
+  /** Exercises
+    */
   // 1 - make this effect fail with a TYPED error
   val aBadFailure = ZIO.succeed[Int](throw new RuntimeException("this is bad!"))
   val aBetterFailure = aBadFailure.sandbox // exposes the defect in the Cause
-  val aBetterFailure_v2 = aBadFailure.unrefine { // surfaces out the exception in the error channel
-    case e => e
-  }
+  val aBetterFailure_v2 =
+    aBadFailure.unrefine { // surfaces out the exception in the error channel
+      case e => e
+    }
 
   // 2 - transform a zio into another zio with a narrower exception type
-  def ioException[R,A](zio: ZIO[R, Throwable, A]): ZIO[R, IOException, A] =
-    zio.refineOrDie {
-      case ioe: IOException => ioe
+  def ioException[R, A](zio: ZIO[R, Throwable, A]): ZIO[R, IOException, A] =
+    zio.refineOrDie { case ioe: IOException =>
+      ioe
     }
 
   // 3
-  def left[R, E, A, B](zio: ZIO[R,E,Either[A,B]]): ZIO[R, Either[E,A], B] =
+  def left[R, E, A, B](zio: ZIO[R, E, Either[A, B]]): ZIO[R, Either[E, A], B] =
     zio.foldZIO(
       e => ZIO.fail(Left(e)),
-      either => either match {
-        case Left(a) => ZIO.fail(Right(a))
-        case Right(b) => ZIO.succeed(b)
-      }
+      either =>
+        either match {
+          case Left(a)  => ZIO.fail(Right(a))
+          case Right(b) => ZIO.succeed(b)
+        }
     )
 
   // 4
@@ -211,16 +225,21 @@ object ZIOErrorHandling extends ZIOAppDefault {
       ZIO.succeed(database.get(userId).map(phone => UserProfile(userId, phone)))
 
   // surface out all the failed cases of this API
-  def betterLookupProfile(userId: String): ZIO[Any, Option[QueryError], UserProfile] =
+  def betterLookupProfile(
+      userId: String
+  ): ZIO[Any, Option[QueryError], UserProfile] =
     lookupProfile(userId).foldZIO(
       error => ZIO.fail(Some(error)),
-      profileOption => profileOption match {
-        case Some(profile) => ZIO.succeed(profile)
-        case None => ZIO.fail(None)
-      }
+      profileOption =>
+        profileOption match {
+          case Some(profile) => ZIO.succeed(profile)
+          case None          => ZIO.fail(None)
+        }
     )
 
-  def betterLookupProfile_v2(userId: String): ZIO[Any, Option[QueryError], UserProfile] =
+  def betterLookupProfile_v2(
+      userId: String
+  ): ZIO[Any, Option[QueryError], UserProfile] =
     lookupProfile(userId).some
 
   override def run = ???
