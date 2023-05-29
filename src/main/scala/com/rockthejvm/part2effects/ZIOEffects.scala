4c835cf4412041e6a908bd6cf9c0209050f65bea
Maciej Gierada <maciej@braidwell.io>
2023-05-29 12:52:09 +0200

    feat: update

--- src/main/scala/com/rockthejvm/part2effects/ZIOEffects.scala
+++ src/main/scala/com/rockthejvm/part2effects/ZIOEffects.scala
@@ -2,103 +2,144 @@ package com.rockthejvm.part2effects
 
 import zio._
 
+import scala.io.StdIn
+
 object ZIOEffects {
 
-  // Notation similar as for simplified ZIO IO
+  // success
   val meaningOfLife: ZIO[Any, Nothing, Int] = ZIO.succeed(42)
-  val aFailure: ZIO[Any, String, Nothing] = ZIO.fail("SWW")
+  // failure
+  val aFailure: ZIO[Any, String, Nothing] = ZIO.fail("Something went wrong")
+  // suspension/delay
   val aSuspendedZIO: ZIO[Any, Throwable, Int] = ZIO.suspend(meaningOfLife)
 
-  // map and flatMap
+  // map + flatMap
   val improvedMOL = meaningOfLife.map(_ * 2)
-  val printingMOL = meaningOfLife.flatMap(v => ZIO.succeed(println(v)))
-
-  // and because be have map and flatMap, we also have for comprehensions
+  val printingMOL = meaningOfLife.flatMap(mol => ZIO.succeed(println(mol)))
+  // for comprehensions
   val smallProgram = for {
-    _ <- ZIO.succeed(println("What's your name?"))
-    name <- ZIO.succeed(scala.io.StdIn.readLine())
-    _ <- ZIO.succeed(println(s"Hello, $name!"))
+    _ <- ZIO.succeed(println("what's your name"))
+    name <- ZIO.succeed(StdIn.readLine())
+    _ <- ZIO.succeed(println(s"Welcome to ZIO, $name"))
   } yield ()
 
-  // A LOT of cobminators
-  // zip
+  // A LOT of combinators
+  // zip, zipWith
   val anotherMOL = ZIO.succeed(100)
-  val tupledZIO = meaningOfLife.zip(anotherMOL) // ZIO[Any, Nothing, (Int, Int)]
-  val conbimedZIO =
-    meaningOfLife.zipWith(anotherMOL)(_ + _) // ZIO[Any, Nothing, Int]
+  val tupledZIO = meaningOfLife.zip(anotherMOL)
+  val combinedZIO = meaningOfLife.zipWith(anotherMOL)(_ * _)
 
-  /** Type aliases for ZIO
-    */
+  /**
+   * Type aliases of ZIOs
+   */
+  // UIO[A] = ZIO[Any,Nothing,A] - no requirements, cannot fail, produces A
+  val aUIO: UIO[Int] = ZIO.succeed(99)
+  // URIO[R,A] = ZIO[R,Nothing,A] - cannot fail
+  val aURIO: URIO[Int, Int] = ZIO.succeed(67)
+  // RIO[R,A] = ZIO[R,Throwable, A] - can fail with a Throwable
+  val anRIO: RIO[Int, Int] = ZIO.succeed(98)
+  val aFailedRIO: RIO[Int, Int] = ZIO.fail(new RuntimeException("RIO failed"))
+  // Task[A] = ZIO[Any, Throwable, A] - no requirements, can fail with a Throwable, produces A
+  val aSuccessfulTask: Task[Int] = ZIO.succeed(89)
+  val aFailedTask: Task[Int] = ZIO.fail(new RuntimeException("Something bad"))
+  // IO[E,A] = ZIO[Any,E,A] - no requirements
+  val aSuccessfulIO: IO[String, Int] = ZIO.succeed(34)
+  val aFailedIO: IO[String, Int] = ZIO.fail("Something bad happened")
 
-  // UIO - ZIO[Any, Nothing, A] - no error, no environment, no requirements,cannot failed, produces A
-  val aUIO: UIO[Int] = ZIO.succeed(42)
 
-  // URIO[R, A] = ZIO[R, Nothing, A] - no error, requires an environment of type R, produces A
-  val aURIO: URIO[String, Int] = ZIO.succeed(42)
+  /**
+   * Exercises
+   */
 
-  // TASK [A] = ZIO[Any, Throwable, A] - no environment, can fail with Throwable, produces A
-  val aSuccessfulTask: Task[Int] = ZIO.succeed(82)
-  val aFailedTask: Task[Int] = ZIO.fail(new RuntimeException("SWW"))
+  // 1 - sequence two ZIOs and take the value of the last one
+  def sequenceTakeLast[R,E,A,B](zioa: ZIO[R,E,A], ziob: ZIO[R,E,B]): ZIO[R,E,B] =
+    zioa.flatMap(_ => ziob.map(b => b))
+  def sequenceTakeLast_v2[R,E,A,B](zioa: ZIO[R,E,A], ziob: ZIO[R,E,B]): ZIO[R,E,B] =
+    for {
+      _ <- zioa
+      b <- ziob
+    } yield b
+  // built-into ZIO
+  def sequenceTakeLast_v3[R,E,A,B](zioa: ZIO[R,E,A], ziob: ZIO[R,E,B]): ZIO[R,E,B] =
+    zioa *> ziob
 
-  // IO[E, A] = ZIO[Any, E, A] - no environment, can fail with E, produces A
-  // val aSuccessfulIO = IO[String, Int](42)
-  // val aFailedIO = IO[String, Int](new RuntimeException("SWW"))
+  // 2 - sequence two ZIOs and take the value of the first one
+  def sequenceTakeFirst[R,E,A,B](zioa: ZIO[R,E,A], ziob: ZIO[R,E,B]): ZIO[R,E,A] =
+    zioa.flatMap(a => ziob.map(_ => a))
+  // built-into ZIO
+  def sequenceTakeFirst_v2[R,E,A,B](zioa: ZIO[R,E,A], ziob: ZIO[R,E,B]): ZIO[R,E,A] =
+    zioa <* ziob
 
-  // RIO[R, A] = ZIO[R, Throwable, A] - requires an environment of type R, can fail with Throwable, produces AS
-  val anRIO: RIO[Int, String] = ZIO.succeed("4")
-  val aFailedRIO: RIO[Int, String] = ZIO.fail(new RuntimeException("SWW"))
+  // 3 - run a ZIO forever
+  def runForever[R,E,A](zio: ZIO[R,E,A]): ZIO[R,E,A] =
+    zio.flatMap(_ => runForever(zio))
+  def runForever_v2[R,E,A](zio: ZIO[R,E,A]): ZIO[R,E,A] =
+    zio *> runForever_v2(zio) // same
 
-  /** ZIO Exercises
-    */
+  val endlessLoop = runForever {
+    ZIO.succeed {
+      println("running...")
+      Thread.sleep(1000)
+    }
+  }
 
-  // 1. Sequence two ZIOs and take the value of the second one
-  def seqnecneTakeLast[R, E, A, B](
-      zioa: ZIO[R, E, A],
-      ziob: ZIO[R, E, B]
-  ): ZIO[R, E, B] = zioa.flatMap(a => ziob.map(b => b))
+  // 4 - convert the value of a ZIO to something else
+  def convert[R,E,A,B](zio: ZIO[R,E,A], value: B): ZIO[R,E,B] =
+    zio.map(_ => value)
+  def convert_v2[R,E,A,B](zio: ZIO[R,E,A], value: B): ZIO[R,E,B] =
+    zio.as(value) // same
 
-  // 2. Sequence two ZIOs and take the value of the first one
-  def sequenceTeakeFirst[R, E, A, B](
-      first: ZIO[R, E, A],
-      second: ZIO[R, E, B]
-  ): ZIO[R, E, A] = first.flatMap(a => second.map(_ => a))
+  // 5 - discard the value of a ZIO to Unit
+  def asUnit[R,E,A](zio: ZIO[R,E,A]): ZIO[R,E,Unit] =
+    convert(zio, ())
+  def asUnit_v2[R,E,A](zio: ZIO[R,E,A]): ZIO[R,E,Unit] =
+    zio.unit // same
 
-  // 3. Run a ZIO forever
-  // def runForever[R, E, A](zio: ZIO[R, E, A]): ZIO[R, E, A] = ???
-  // val endlessLoop = runForever {
-  //   ZIO.succeed {
-  //     Thread.sleep(100)
-  //   }
-  // }
+  // 6 - recursion
+  def sum(n: Int): Int =
+    if (n == 0) 0
+    else n + sum(n - 1) // will crash at sum(20000)
 
-  // 4. Convert the value of a ZIO to something else
-  // def conver[R, E, A, B](zio: ZIO[R, A, B], value: B): ZIO[R, E, B] = ???
+  def sumZIO(n: Int): UIO[Int] =
+    if (n == 0) ZIO.succeed(0)
+    else for {
+      current <- ZIO.succeed(n)
+      prevSum <- sumZIO(n - 1)
+    } yield current + prevSum
 
-  // 5. Discard the value of a ZIO
-  // def asUnit[R, A, E](zio: ZIO[R, E, A]): ZIO[R, E, Unit] = ???
+  // 7 - fibonacci
+  // hint: use ZIO.suspend/ZIO.suspendSucceed
+  def fibo(n: Int): BigInt =
+    if (n <= 2) 1
+    else fibo(n - 1) + fibo(n - 2)
+
+  def fiboZIO(n: Int): UIO[BigInt] =
+    if (n <= 2) ZIO.succeed(1)
+    else for {
+      last <- ZIO.suspendSucceed(fiboZIO(n - 1))
+      prev <- fiboZIO(n - 2)
+    } yield last + prev
 
   def main(args: Array[String]): Unit = {
     val runtime = Runtime.default
+    given trace: Trace = Trace.empty
+    Unsafe.unsafeCompat { (u: Unsafe) =>
+      given uns: Unsafe = u
 
-    val firstEffect = ZIO.succeed {
-      println("Computing first effect")
-      Thread.sleep(1000)
-      1
+      val firstEffect = ZIO.succeed {
+        println("computing first effect...")
+        Thread.sleep(1000)
+        1
+      }
+
+      val secondEffect = ZIO.succeed {
+        println("computing second effect...")
+        Thread.sleep(1000)
+        2
+      }
+
+      println(runtime.unsafe.run(sumZIO(20000)))
     }
-
-    val secondEffect = ZIO.succeed {
-      println("Computing second effect")
-      Thread.sleep(1000)
-      2
-    }
-
-    val program =
-      for result <- seqnecneTakeLast(firstEffect, secondEffect)
-      yield result
-
-    val programResult =  program.runtime.unsafe()
-    println(programResult)
-
   }
-
 }
+
